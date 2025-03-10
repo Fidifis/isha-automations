8984827f12ec518400b98fc963f770b5f253a969
Filip Digrín <fdigrin@trask.cz>
2025-03-10 13:04:36 +0100

    Some basic

---
+++ flake.nix
@@ -0,0 +1,25 @@
+# What are Nix Flakes https://www.tweag.io/blog/2020-05-25-flakes/
+{
+  description = "A very basic flake for infra development";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pulumi
+            pulumiPackages.pulumi-language-nodejs
+            nodejs
+            go
+          ];
+        };
+      }
+    );
+}
