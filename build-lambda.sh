0197b49a296e5be8d864fc2891a9d6b67fe03c64
Filip Digrín <fdigrin@trask.cz>
2025-05-12 12:06:24 +0200

    move to scripts

--- build-lambda.sh
+++ build-lambda.sh
@@ -11,14 +11,16 @@ mkdir -p bin
 
 $engine run --rm \
   -v ./code:/build/code:ro \
+  -v ./scripts:/build/scripts:ro \
   -v ./bin:/build/bin \
   -w /build \
   golang:1.24 \
-  bash -c "apt-get update && apt-get install -y zip jq && /build/code/build-go.sh"
+  bash -c "apt-get update && apt-get install -y zip jq && /build/scripts/build-go.sh"
 
 $engine run --rm \
   -v ./code:/build/code:ro \
+  -v ./scripts:/build/scripts:ro \
   -v ./bin:/build/bin \
   -w /build \
   debian \
-  bash -c "apt-get update && apt-get install -y zip jq curl xz-utils && /build/code/build-special.sh"
+  bash -c "apt-get update && apt-get install -y zip jq curl xz-utils && /build/scripts/build-special.sh"
