d5ab72bc0ea8fad0e4653381d678ee8edbdff2d9
Fidifis <git@fidifis.com>
2025-06-16 12:30:45 +0200

    rest api

--- pulumi/components/apiGateway.ts
+++ pulumi/components/apiGateway.ts
@@ -12,7 +12,7 @@ export interface ApiGatewayRoute {
   payloadFormatVersion?: Input<string>;
 }
 
-export interface ApiGatewayV2Props {
+export interface ApiGatewayProps {
   name?: Input<string>;
   tags: aws.Tags;
   description?: Input<string>;
@@ -32,7 +32,7 @@ export interface ApiGatewayV2Props {
   };
 }
 
-export default class ApiGatewayV2 extends pulumi.ComponentResource {
+export class ApiGatewayV2 extends pulumi.ComponentResource {
   public readonly apiGateway: aws.apigatewayv2.Api;
   public readonly stage: aws.apigatewayv2.Stage;
   public readonly integrations: aws.apigatewayv2.Integration[] = [];
@@ -44,7 +44,7 @@ export default class ApiGatewayV2 extends pulumi.ComponentResource {
 
   constructor(
     name: string,
-    args: ApiGatewayV2Props,
+    args: ApiGatewayProps,
     opts?: pulumi.ComponentResourceOptions,
   ) {
     super("fidifis:aws:ApiGatewayV2", name, {}, opts);
@@ -207,6 +207,236 @@ export default class ApiGatewayV2 extends pulumi.ComponentResource {
       premissions: this.permissions,
       domain: this.domain,
       certificate: this.certificate,
+    });
+  }
+}
+
+export class RestApiGateway extends pulumi.ComponentResource {
+  public readonly apiGateway: aws.apigateway.RestApi;
+  public readonly deployment: aws.apigateway.Deployment;
+  public readonly stage: aws.apigateway.Stage;
+  public readonly resources: aws.apigateway.Resource[] = [];
+  public readonly methods: aws.apigateway.Method[] = [];
+  public readonly integrations: aws.apigateway.Integration[] = [];
+  public readonly permissions: aws.lambda.Permission[] = [];
+  public readonly domain?: aws.apigateway.DomainName;
+  public readonly certificate?: aws.acm.Certificate;
+  public readonly basePathMapping?: aws.apigateway.BasePathMapping;
+  private readonly authorizers: aws.apigateway.Authorizer[] = [];
+
+  constructor(
+    name: string,
+    args: ApiGatewayProps,
+    opts?: pulumi.ComponentResourceOptions,
+  ) {
+    super("fidifis:aws:RestApiGateway", name, {}, opts);
+
+    this.apiGateway = new aws.apigateway.RestApi(
+      name,
+      {
+        name: args.name,
+        description: args.description,
+        tags: args.tags,
+        endpointConfiguration: {
+          types: "REGIONAL",
+        },
+      },
+      { parent: this },
+    );
+
+    args.routes.forEach((route, index) => {
+      const method = route.method ?? "POST";
+      const pathParts = route.path.toString().split('/').filter(part => part !== '');
+      
+      let currentResource: pulumi.Output<string> = this.apiGateway.rootResourceId;
+      let resourcePath = "";
+
+      // Create nested resources for path segments
+      pathParts.forEach((part, partIndex) => {
+        resourcePath += `/${part}`;
+        const resourceName = `${name}-Res-${index}-${partIndex}`;
+        
+        const resource = new aws.apigateway.Resource(
+          resourceName,
+          {
+            restApi: this.apiGateway.id,
+            parentId: currentResource,
+            pathPart: part,
+          },
+          { parent: this },
+        );
+        
+        this.resources.push(resource);
+        currentResource = resource.id;
+      });
+
+      // Create authorizer if needed
+      let authorizer: aws.apigateway.Authorizer | null = null;
+      if (route.authorizer) {
+        authorizer = new aws.apigateway.Authorizer(
+          `${name}-Authorizer-${index}`,
+          {
+            name: `${name}-authorizer-${index}`,
+            restApi: this.apiGateway.id,
+            authorizerUri: route.authorizer.invokeArn,
+            authorizerCredentials: route.execRole?.arn,
+            type: "REQUEST",
+            identitySource: "method.request.header.Authorization",
+          },
+          { parent: this },
+        );
+        this.authorizers.push(authorizer);
+
+        // Permission for authorizer
+        const authorizerPermission = new aws.lambda.Permission(
+          `${name}-AuthPermission-${index}`,
+          {
+            action: "lambda:InvokeFunction",
+            function: route.authorizer.name,
+            principal: "apigateway.amazonaws.com",
+            sourceArn: pulumi.interpolate`${this.apiGateway.executionArn}/*`,
+          },
+          { parent: this },
+        );
+        this.permissions.push(authorizerPermission);
+      }
+
+      // Create method
+      const apiMethod = new aws.apigateway.Method(
+        `${name}-Method-${index}`,
+        {
+          restApi: this.apiGateway.id,
+          resourceId: currentResource,
+          httpMethod: method,
+          authorization: route.authorizer ? "CUSTOM" : "NONE",
+          authorizerId: authorizer?.id,
+        },
+        { parent: this },
+      );
+      this.methods.push(apiMethod);
+
+      // Create integration
+      const integrationConfig = route.eventHandler instanceof aws.sfn.StateMachine
+        ? {
+            type: "AWS",
+            integrationHttpMethod: "POST",
+            uri: pulumi.interpolate`arn:aws:apigateway:${aws.getRegionOutput().name}:states:action/${route.stateMachineStartSync ? "StartSyncExecution" : "StartExecution"}`,
+            credentials: route.execRole?.arn,
+            requestTemplates: {
+              "application/json": pulumi.interpolate`{
+                "input": "$util.escapeJavaScript($input.json('$'))",
+                "stateMachineArn": "${route.eventHandler.arn}"
+              }`,
+            },
+          }
+        : {
+            type: "AWS_PROXY",
+            integrationHttpMethod: "POST",
+            uri: route.eventHandler.invokeArn,
+          };
+
+      const integration = new aws.apigateway.Integration(
+        `${name}-Integration-${index}`,
+        {
+          restApi: this.apiGateway.id,
+          resourceId: currentResource,
+          httpMethod: apiMethod.httpMethod,
+          ...integrationConfig,
+        },
+        { parent: this },
+      );
+      this.integrations.push(integration);
+
+      if (route.eventHandler instanceof aws.lambda.Function) {
+        const permission = new aws.lambda.Permission(
+          `${name}-Permission-${index}`,
+          {
+            action: "lambda:InvokeFunction",
+            function: route.eventHandler.name,
+            principal: "apigateway.amazonaws.com",
+            sourceArn: pulumi.interpolate`${this.apiGateway.executionArn}/*`,
+          },
+          { parent: this },
+        );
+        this.permissions.push(permission);
+      }
+    });
+
+    this.deployment = new aws.apigateway.Deployment(
+      `${name}-Deployment`,
+      {
+        restApi: this.apiGateway.id,
+        description: "Deployment for REST API",
+      },
+      { 
+        parent: this,
+        dependsOn: [...this.methods, ...this.integrations]
+      },
+    );
+
+    // Create stage
+    this.stage = new aws.apigateway.Stage(
+      `${name}-Stage`,
+      {
+        restApi: this.apiGateway.id,
+        deployment: this.deployment.id,
+        stageName: args.stage?.name ?? "prod",
+        tags: args.tags,
+      },
+      { parent: this },
+    );
+
+    // Handle custom domain if provided
+    if (args.domain) {
+    this.certificate = new aws.acm.Certificate(
+      `${name}-Certificate`,
+      {
+        domainName: args.domain,
+        validationMethod: "DNS",
+        tags: args.tags,
+      },
+      { parent: this },
+    );
+
+    this.domain = new aws.apigateway.DomainName(
+      `${name}-Domain`,
+      {
+        domainName: args.domain,
+        certificateArn: this.certificate.arn,
+        endpointConfiguration: {
+          types: "REGIONAL",
+        },
+        tags: args.tags,
+      },
+      { parent: this },
+    );
+
+    this.basePathMapping = new aws.apigateway.BasePathMapping(
+      `${name}-Mapping`,
+      {
+        restApi: this.apiGateway.id,
+        stageName: this.stage.stageName,
+        domainName: this.domain.domainName,
+      },
+      { parent: this },
+    );
+    }
+
+    if (args.corsConfig) {
+      throw "cors not implemented"
+    }
+
+    this.registerOutputs({
+      apiGateway: this.apiGateway,
+      deployment: this.deployment,
+      stage: this.stage,
+      resources: this.resources,
+      methods: this.methods,
+      integrations: this.integrations,
+      permissions: this.permissions,
+      domain: this.domain,
+      certificate: this.certificate,
+      basePathMapping: this.basePathMapping,
     });
   }
 }
